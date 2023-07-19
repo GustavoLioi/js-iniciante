09098774dc78a9aa1c61f76b86ce2aba1cef9f0d
Gustavo Lioi <105881795+GustavoLioi@users.noreply.github.com>
2023-07-19 14:51:35 -0300

    js

--- primeiro_arquivo.js
+++ primeiro_arquivo.js
@@ -1,1 +1,13 @@
-console.log("JAVASCRIPT");
+let teste= "minha string";
+teste = 10;
+console.log("teste");
+
+let teste2 = 20;
+let teste3;
+
+console.log(teste2);
+teste3 = 30;
+console.log(texte3);
+
+const teste4 ="eu sou imutavel";
+console.log(teste4);
